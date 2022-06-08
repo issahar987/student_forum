b8e2ac266702b383af8833d1f790ae150754451d
issahar987 <issahar987@gmail.com>
2022-06-08 20:41:13 +0200

    step 1: generate and send token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,45 @@
 class SessionsController < ApplicationController
-  def new
+    skip_before_action :verify_authenticity_token
+    def new
   end
 
   def create
-    user = User.find_by(login: params[:session][:login])
-    if user && user.authenticate(params[:session][:password])
-      # Wszystko dobrze, logujemy
-      log_in user
-      redirect_to user
-    else
-      # Niedobrze
-      render 'new'
+    # user = User.find_by(login: params[:session][:login])
+    # if user && user.authenticate(params[:session][:password])
+    #   # Wszystko dobrze, logujemy
+    #   log_in user
+    #   redirect_to user
+    # else
+    #   # Niedobrze
+    #   render 'new'
+    # end
+    respond_to do |format|
+      user = User.find_by(login: params[:session][:login])
+      if user && user.authenticate(params[:session][:password])
+        format.html do
+          log_in user
+          redirect_to user
+        end
+        format.json do
+          user.password = params[:session][:password]
+          user.regenerate_token
+          render json: { token: user.token }
+        end
+      else
+        format.html do
+          render 'new'
+      end
+      format.json do
+        render json: { message: 'Niepoprawne dane' }
+      end
     end
   end
+end
   
   def destroy
     log_out
     redirect_to root_url
   end
+
+
 end
